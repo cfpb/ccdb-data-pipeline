64e82031d9eea9db0fef4a499428b1c0c36721e1
Adam Zarger <adam.zarger@cfpb.gov>
2017-08-25 10:23:05 -0400

    Fixed typo in debugging code

--- complaints/streamParser.py
+++ complaints/streamParser.py
@@ -68,7 +68,7 @@ def parse_json_file(input_file_name, output_file_name, logger):
             if line_counter > 0:
                 line_count_total += line_counter
                 logger.info("Processed {} lines, {} total".format(line_counter, line_count_total))
-        except IncompleteJSONError as e:
+        except ijson.common.IncompleteJSONError as e:
             logger.info('IncompleteJSONError: {}'.format(e))
 
 

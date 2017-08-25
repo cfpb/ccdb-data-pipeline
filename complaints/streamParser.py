02210e68ea9bd1ea1f1dafe0805c94c7246f937b
Adam Zarger <adam.zarger@cfpb.gov>
2017-08-25 10:07:58 -0400

    Added temporary debugging code to determine cause of parsing error

--- complaints/streamParser.py
+++ complaints/streamParser.py
@@ -43,30 +43,33 @@ def parse_json_file(input_file_name, output_file_name, logger):
         my_data_array = []
         my_column_array = []
 
-        for prefix, event, value in parser:
-            if prefix == 'data.item.item':
-                my_data_array.append(value)
-            elif (prefix, event) == ('data.item', 'start_array'):
-                continue
-                n += 1
-            elif (prefix, event) == ('data.item', 'end_array'):
-                new_complaint = dict(zip(my_column_array, my_data_array))
-                new_complaint["has_narrative"] = (not (not new_complaint["complaint_what_happened"] or len(new_complaint["complaint_what_happened"]) == 0))
-                my_data_array = []
-                target.write(json.dumps(new_complaint))
-                target.write('\n')
-            elif prefix == 'meta.view.columns.item.fieldName':
-                my_column_array.append(value)
+        try:
+            for prefix, event, value in parser:
+                if prefix == 'data.item.item':
+                    my_data_array.append(value)
+                elif (prefix, event) == ('data.item', 'start_array'):
+                    continue
+                    n += 1
+                elif (prefix, event) == ('data.item', 'end_array'):
+                    new_complaint = dict(zip(my_column_array, my_data_array))
+                    new_complaint["has_narrative"] = (not (not new_complaint["complaint_what_happened"] or len(new_complaint["complaint_what_happened"]) == 0))
+                    my_data_array = []
+                    target.write(json.dumps(new_complaint))
+                    target.write('\n')
+                elif prefix == 'meta.view.columns.item.fieldName':
+                    my_column_array.append(value)
 
-            line_counter += 1
-            if line_counter >= 100000:
+                line_counter += 1
+                if line_counter >= 100000:
+                    line_count_total += line_counter
+                    logger.info("Processed {} lines, {} total".format(line_counter, line_count_total))
+                    line_counter = 0
+
+            if line_counter > 0:
                 line_count_total += line_counter
                 logger.info("Processed {} lines, {} total".format(line_counter, line_count_total))
-                line_counter = 0
-
-        if line_counter > 0:
-            line_count_total += line_counter
-            logger.info("Processed {} lines, {} total".format(line_counter, line_count_total))
+        except IncompleteJSONError as e:
+            logger.info('IncompleteJSONError: {}'.format(e))
 
 
     target.close()

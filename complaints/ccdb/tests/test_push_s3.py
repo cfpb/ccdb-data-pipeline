60bb8c9db8a93d854c9073383bd24d0848c1025b
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2019-09-30 13:42:51 -0400

    test: Cover push_s3.py

---
+++ complaints/ccdb/tests/test_push_s3.py
@@ -0,0 +1,55 @@
+from __future__ import unicode_literals
+
+import os
+import unittest
+
+import complaints.ccdb.push_s3 as sut
+from common.tests import build_argv, captured_output
+
+try:
+    from unittest.mock import patch, Mock, ANY
+except ImportError:
+    from mock import patch, Mock, ANY
+
+
+def toAbsolute(relative):
+    # where is _this_ file?
+    thisScriptDir = os.path.dirname(__file__)
+
+    return os.path.join(thisScriptDir, relative)
+
+
+# ------------------------------------------------------------------------------
+# Classes
+# ------------------------------------------------------------------------------
+
+class TestMain(unittest.TestCase):
+    def setUp(self):
+        self.optional = [
+            '--s3-bucket', 'foo',
+            '--s3-folder', 'bar'
+        ]
+        self.positional = [
+            toAbsolute('__fixtures__/from_s3.ndjson')
+        ]
+
+    @patch('complaints.ccdb.push_s3.boto3')
+    def test_main_happy_path(self, boto3):
+        bucket = Mock()
+        s3 = Mock()
+        s3.Bucket.return_value = bucket
+        boto3.resource.return_value = s3
+
+        argv = build_argv(self.optional, self.positional)
+        with captured_output(argv) as (out, err):
+            sut.main()
+
+        boto3.resource.assert_called_once_with('s3')
+        s3.Bucket.assert_called_once_with('foo')
+        bucket.upload_file.assert_called_once_with(
+            self.positional[0], 'bar/from_s3.ndjson', Callback=ANY
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()

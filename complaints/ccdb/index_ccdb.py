2afe035ee97dad6729d4758ef56673e64c458e12
Seph Coster <joseph.coster@cfpb.gov>
2020-11-25 16:30:41 -0500

    no os.environ

--- complaints/ccdb/index_ccdb.py
+++ complaints/ccdb/index_ccdb.py
@@ -238,6 +238,8 @@ def build_arg_parser():
               help="Complaint data in NDJSON format")
     group.add('--metadata', dest='metadata',
               help="Metadata in JSON format")
+    group.add('--is-aws-host',  dest='is_aws_host',
+              help='Is your ES instance hosted as an AWS service?')
     return p
 
 
@@ -256,6 +258,7 @@ def main():
     backup_index_name = "{}-v2".format(index_alias)
 
     logger.info("Creating Elasticsearch Connection")
+    
     if cfg.is_aws_host:
         es = get_aws_es_connection(cfg)
         logger.info('AWS configured as Elasticsearch host')

693d46fb1ee5e7a2adf59f1380735f5b6100bb18
Andy Chosak <andy.chosak@cfpb.gov>
2025-08-15 13:31:36 -0400

    Properly pass HTTP auth parameters to ES 7

--- common/tests/test_es_proxy.py
+++ common/tests/test_es_proxy.py
@@ -22,7 +22,23 @@ class TestEdges(unittest.TestCase):
         actual = sut.get_es_connection(options)
         self.assertEqual(actual, 'passed')
         mock_es.assert_called_once_with(
-            'http://www.example.org:9222',
-            http_auth=('king', 'kong'),
+            'http://king:kong@www.example.org:9222',
             timeout=2000,
-            user_ssl=True)
+            use_ssl=False)
+
+    @patch('common.es_proxy.Elasticsearch')
+    def test_get_es_connection_ssl(self, mock_es):
+        options = make_configargs({
+            'es_host': 'www.example.org',
+            'es_port': '443',
+            'es_username': 'king!',
+            'es_password': 'kong',
+        })
+        mock_es.return_value = 'passed'
+
+        actual = sut.get_es_connection(options)
+        self.assertEqual(actual, 'passed')
+        mock_es.assert_called_once_with(
+            'http://king%21:kong@www.example.org:443',
+            timeout=2000,
+            use_ssl=True)

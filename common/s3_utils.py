cf876b14a5a496447167b9442152963a9e6b19f0
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2026-05-31 23:00:44 -0700

    Make zipfile behavior match upsert behavior of indexing

--- common/s3_utils.py
+++ common/s3_utils.py
@@ -6,10 +6,10 @@ from zipfile import ZIP_DEFLATED, ZipFile
 import boto3
 
 
-def download_file(bucket, key, file):
+def download_file(bucket, prefix, file):
     s3 = boto3.resource("s3")
     bkt = s3.Bucket(bucket)
-    bkt.download_file(key, file)
+    bkt.download_file(f"{prefix}{file}", file)
 
 
 def append_to_zip(zip_name, datafile):
@@ -18,35 +18,59 @@ def append_to_zip(zip_name, datafile):
     with ZipFile(zip_name, "r") as zip_ref:
         zip_ref.extractall(".")
 
-    write_and_zip(datafile, filename, "a")
+    new_ids = {}
+
+    with open("temp_file", "w", newline="") as tempfile:
+        with open(filename, "r", newline="") as original_file:
+            orig_reader = csv.reader(original_file)
+            tempfile.writerow(next(orig_reader))
+
+            with open(datafile, "r", newline="") as file_to_append:
+                append_reader = csv.reader(file_to_append)
+                next(append_reader)
+                for row in append_reader:
+                    new_ids[row[15]] = 1
+                    tempfile.writerow(row)
+
+            for row in orig_reader:
+                if new_ids.get(row[15]):
+                    continue
+                tempfile.writerow(row)
+
+    os.replace("temp_file", filename)
+
+    make_zip(filename)
 
 
-def upload_file(bucket, key, file):
+def upload_file(bucket, prefix, file):
     s3 = boto3.resource("s3")
     bkt = s3.Bucket(bucket)
-    bkt.upload_file(file, key)
+    bkt.upload_file(file, f"{prefix}{file}")
 
 
-def update_zipped_archive(bucket, key, new_data):
-    _, zip_name = os.path.split(key)
-    download_file(bucket, key, zip_name)
-    append_to_zip(zip_name, new_data)
-    upload_file(bucket, key, zip_name)
+def update_zipped_archives(bucket, prefix, basename, new_data):
+    csv_filename = f"{basename}.csv"
+    zipped_csv = f"{csv_filename}.zip"
+
+    download_file(bucket, prefix, zipped_csv)
+    append_to_zip(zipped_csv, new_data)
+    upload_file(bucket, prefix, zipped_csv)
 
 
-def create_zipped_archive(bucket, key, new_data):
-    _, zip_name = os.path.split(key)
-    filename = zip_name[:-4]
+def create_zipped_archives(bucket, prefix, basename, new_data):
+    csv_filename = f"{basename}.csv"
+    zipped_csv = f"{csv_filename}.zip"
 
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     header_file = os.path.join(base_dir, "complaints/ccdb/fields-s3/v1-csv.txt")
     with open(header_file, "r") as f:
         public_header = f.read().splitlines()
-        write_and_zip(new_data, filename, header=public_header)
-    upload_file(bucket, key, zip_name)
+        write_csv(new_data, csv_filename, header=public_header)
+    make_zip(csv_filename, zipped_csv)
+    upload_file(bucket, prefix, zipped_csv)
 
 
-def write_and_zip(in_file, out_file, mode="w", header=None):
+def write_csv(in_file, out_file, mode="w", header=None):
     with open(in_file, "r", newline="") as infile:
         with open(out_file, mode, newline="") as outfile:
             reader = csv.reader(infile)
@@ -63,8 +87,10 @@ def write_and_zip(in_file, out_file, mode="w", header=None):
                     row[12] = ymd(row[12])
                 writer.writerow(row)
 
-    with ZipFile(f"{out_file}.zip", "w", ZIP_DEFLATED) as zip:
-        zip.write(out_file)
+
+def make_zip(file, zipped):
+    with ZipFile(zipped, "w", ZIP_DEFLATED) as zip:
+        zip.write(file)
 
 
 def ymd(iso):

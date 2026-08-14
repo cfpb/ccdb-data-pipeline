2b07440f817652372a4ef6517f192d05939a6d97
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2026-08-14 14:41:09 -0700

    Fixup tests

--- common/tests/test_s3_utils.py
+++ common/tests/test_s3_utils.py
@@ -88,7 +88,6 @@ class TestS3Utils(unittest.TestCase):
                 "",
                 "",
                 "",
-                "",
                 "2026-01-02T00:00:00",
             ]
         ]
@@ -105,7 +104,7 @@ class TestS3Utils(unittest.TestCase):
 
         mock_writer_instance.writerow.assert_any_call(["h1", "h2"])
         mock_writer_instance.writerow.assert_any_call(
-            ["2026-01-01", "data", "", "", "", "", "", "", "", "", "", "", "2026-01-02"]
+            ["2026-01-01", "data", "", "", "", "", "", "", "", "", "", "2026-01-02"]
         )
 
     @patch("common.s3_utils.make_zip")
